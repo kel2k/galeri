b497caf4bbe614d7e41dd38afe2dfaad2f42f530
kel2k <cytichax@gmail.com>
2024-01-22 10:23:06 +0700

    galeri

---
+++ app/Views/dashboard.php
@@ -0,0 +1,25 @@
+<div class="content-body">
+
+    <div class="container-fluid mt-3">
+        <div class="row">
+            <div class="col-lg-3 col-sm-6">
+                <div class="card gradient-1">
+                    <div class="card-body">
+                        <h3 class="card-title text-white">Total Users</h3>
+                        <div class="d-inline-block">
+                            <h2 class="text-white">
+                                <?php echo $totalUsers; ?>
+                            </h2>
+                            <p class="text-white mb-0">Jan- December 2023</p>
+                        </div>
+                        <span class="float-right display-5 opacity-5"><i class="fa fa-address-card"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+</div>
+</div>
+</div>
